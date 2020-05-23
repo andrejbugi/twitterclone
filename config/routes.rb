bd4fc8063f5f864c0b637efbc6356790ae7c7f00
andrejbugi <andrejbugi96@gmail.com>
2020-05-23 20:22:09 +0200

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+  resources :tweets
+
+  root to: 'tweets#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
