d9c827f4f0536bd7da17ea1eda66a3554907cf48
andrejbugi <andrejbugi96@gmail.com>
2020-05-24 12:35:34 +0200

    CRUD actions and views add

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,43 +1,50 @@
 class TweetsController < ApplicationController
   def index
-    tweets = Tweet.order('created_at DESC');
-    render json: { status: 'SUCCESS', message: 'Loaded tweets', data:tweets }, status: :ok
+    @tweets = Tweet.all
   end
 
   def show
-    tweet = Tweet.find(params[:id])
-    render json: { status: 'SUCCESS', message: 'Loaded tweet', data:tweet }, status: :ok
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
   end
 
   def create
-    tweet = Tweet.new(tweet_params)
+    @tweet = Tweet.new(tweet_params)
 
-    if tweet.save
-      render json: { status: 'SUCCESS', message: 'Saved tweet', data:tweet }, status: :ok
+    if @tweet.save
+      redirect_to tweet_path(@tweet)
     else
-      render json: { status: 'ERROR', message: 'Tweet not saved', data:tweet.errors }, status: :unprocessable_entity
+      render :new
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path(@tweet)
+    else
+      render :edit
     end
   end
 
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy
-    render json: { status: 'SUCCESS', message: 'Deleted tweet', data:tweet }, status: :ok
-  end
 
-  def update
-    tweet = Tweet.find(params[:id])
-    if tweet.update(tweet_params)
-      render json: { status: 'SUCCESS', message: 'Updated tweet', data:tweet }, status: :ok
-    else
-      render json: { status: 'ERROR', message: 'tweet not updated', data:tweet }, status: :unprocessable_entity
-    end
+    redirect_to tweets_path
   end
 
   private
 
   def tweet_params
-    params.permit(:title, :body)
+    params.require(:tweet).permit(:title, :body)
   end
 
 end
