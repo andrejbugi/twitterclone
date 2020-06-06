b1ac7bc2ab4ba55a1e4b7d082c8e8e34f25a07b2
andrejbugi <andrejbugi96@gmail.com>
2020-06-06 16:38:53 +0200

    add validations on tweet's body maximum length

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
 class Tweet < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 140 }
 end
