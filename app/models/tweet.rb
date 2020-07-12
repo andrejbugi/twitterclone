87f95e63e335763ee63e12e875c3a34ced787e02
andrejbugi <andrejbugi96@gmail.com>
2020-07-12 23:29:52 +0200

    add option for deleting comments

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { maximum: 140 }
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
